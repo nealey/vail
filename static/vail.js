55dbdf8635e2d616601123afd356eaba16419456
Neale Pickett <neale@woozle.org>
2020-04-10 08:27:35 -0600

    Ready to beta?

--- static/vail.js
+++ static/vail.js
@@ -3,14 +3,15 @@ var gain = ac.createGain()
 gain.connect(ac.destination)
 gain.gain.value = 0.1
 
-var longest = 500
+var short = 80
+var long = 200
 var audioFreq = 660
 var audioFreqMe = audioFreq * 6 / 5 // I think this works out to a minor third
 var myosc
 
 function message(event) {
   let duration = Number(event.data) || 0
-  duration = Math.min(duration, longest)
+  duration = Math.min(duration, long)
   
   let osc = ac.createOscillator()
   osc.connect(gain)
@@ -20,37 +21,37 @@ function message(event) {
 }
 
 function key(event) {
-  if (event.type.endsWith("down")) {
-    if (! event.repeat) {
-      window.down = event.timeStamp
-    }
-    if (! myosc) {
-      myosc = ac.createOscillator()
-      myosc.connect(gain)
-      myosc.frequency.value = audioFreqMe
-      myosc.start(ac.currentTime)
-    }
-  } else {
-    let duration = event.timeStamp - window.down
-    duration = Math.min(duration, longest)
-    console.log(event.timeStamp, window.down, duration)
-    window.socket.send(duration)
-    
-    if (myosc) {
-      myosc.stop(ac.currentTime)
-      myosc = null
-    }
+  let duration = 0
+
+  if ((event.button === 0) || (event.key == ",") || (event.key == "w")) {
+    duration = short
   }
+  if ((event.button === 2) || (event.key == ".") || (event.key == "v")) {
+    duration = long
+  }
+  // You don't get to hold the key down yet, sorry
+  if ((event.repeat) || (duration === 0)) {
+    return
+  }
+
+  window.socket.send(duration)
+
+  myosc = ac.createOscillator()
+  myosc.connect(gain)
+  myosc.frequency.value = audioFreqMe
+  myosc.start(ac.currentTime)
+  myosc.stop(ac.currentTime + duration * 0.001)
 }
 
 function init() {
-  window.socket = new WebSocket("ws://penguin.linux.test:8080/chat")
+  let wsUrl = new URL(window.location)
+  wsUrl.protocol = "ws:"
+  wsUrl.pathname += "chat"
+  window.socket = new WebSocket(wsUrl)
   window.socket.addEventListener("message", message)
 
   document.addEventListener("mousedown", e => key(e))
-  document.addEventListener("mouseup", e => key(e))
   document.addEventListener("keydown", e => key(e))
-  document.addEventListener("keyup", e => key(e))
 }
 
 
